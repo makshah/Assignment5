b26319207a461cecb926d564b2ebe0fc9891135c
makshah <meetshah1997@gmail.com>
2020-10-11 11:37:58 -0700

    Initial Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-var {src, dest, watch} = require('gulp');
-var sass = require('gulp-sass');
-sass.compiler = require('node-sass');
+var {src, dest, watch} = require("gulp");
+var sass = require("gulp-sass");
+sass.compiler = require("node-sass");
 
-function css () {
-    return src('src/*.scss')
-        .pipe(sass())
-        .pipe(dest('dist/css'))
-};
+function css() {
+	return src("src/*.scss")
+	.pipe(sass())
+	.pipe(dest("dist/css"))
+}
 
 exports.default = function () {
-    watch('src/*.scss', css);
-    watch('src/modules/*.scss', css);
-};
+	watch("src/*.scss", css);
+}
